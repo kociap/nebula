b08c76b8ee28fd87b35bee277e5ea85de966b710
sebawoz02 <sebawoz2002@gmail.com>
2023-10-13 11:19:28 +0200

    GLFW configuration

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,32 @@
-#include "project/tmp.hpp"
+#include <project/tmp.hpp>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   (void) argc;
   (void) argv;
 
-  if(tmp::add(2, 2) != 4)
+  // Initialize GLFW
+  if (!glfwInit()) {
     return -1;
+  }
+  // Create a GLFW window
+  GLFWwindow* window = glfwCreateWindow(tmp::add(400, 400), 600, "Hello GLFW", nullptr, nullptr);
+
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return 0;
 }
+
+
--- src/tmp.cpp
+++ src/tmp.cpp
@@ -1,3 +1,3 @@
-#include "project/tmp.hpp"
+#include <project/tmp.hpp>
 
 int tmp::add(int a, int b) { return a + b; }
