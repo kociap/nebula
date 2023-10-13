5e11be4a5db039c37ec951e8e94f57b24f8bacad
sebawoz02 <sebawoz2002@gmail.com>
2023-10-13 10:29:27 +0200

    Creating a trivial cmake
    GLFW configuration
    
    Added main_ci action

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nebula
+
+[![MAIN-CI](https://https://github.com/kociap/nebula/actions/workflows/main_ci.yaml/badge.svg?branch=main)](https://https://github.com/kociap/nebula/actions/workflows/main_ci.yaml)
---
+++ scripts/config_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if CMake is installed
+if ! command -v cmake &>/dev/null; then
+    echo "CMake is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y cmake
+else
+    echo "CMake is already installed."
+fi
+
+if ! dpkg -l | grep -q libglfw3-dev; then
+    echo "GLFW development libraries are not installed. Installing..."
+    sudo apt update
+    sudo apt-get install -y libglfw3 libglfw3-dev
+else
+    echo "GLFW development libraries are already installed."
+fi
+
+echo "Installation complete."
