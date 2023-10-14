558f7252a574277690d8f2c8eb049181b84a2d1c
Piotr Kocia <peter.kocia@gmail.com>
2023-10-15 01:10:16 +0200

    fix CI not installing GLFW dependencies

--- scripts/config_env.sh
+++ scripts/config_env.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
+sudo apt-get update
+
 # Check if CMake is installed
 if ! command -v cmake &>/dev/null; then
     echo "CMake is not installed. Installing..."
-    sudo apt update
-    sudo apt install -y cmake
+    sudo apt-get update
+    sudo apt-get install -y cmake
 else
     echo "CMake is already installed."
 fi
 
-if ! dpkg -l | grep -q libglfw3-dev; then
-    echo "GLFW development libraries are not installed. Installing..."
-    sudo apt update
-    sudo apt-get install -y libglfw3 libglfw3-dev
-else
-    echo "GLFW development libraries are already installed."
-fi
+# GLFW dependencies.
+sudo apt-get install -y libxinerama-dev libxcursor-dev libxi-dev
 
 echo "Installation complete."
