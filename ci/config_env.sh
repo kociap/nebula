4c554feef227d71119b2f98d063af76cee6a3531
sebawoz02 <sebawoz2002@gmail.com>
2023-10-15 21:37:27 +0200

    Changed compiler in CI to clang.

--- ci/config_env.sh
+++ ci/config_env.sh
@@ -5,10 +5,17 @@ sudo apt-get update
 # Check if CMake is installed.
 if ! command -v cmake &>/dev/null; then
     echo "CMake is not installed. Installing..."
-    sudo apt-get update
     sudo apt-get install -y cmake
 else
     echo "CMake is already installed."
+fi
+
+# Check if Clang is installed.
+if ! command -v clang &>/dev/null; then
+    echo "Clang is not installed. Installing..."
+    sudo apt-get install -y clang
+else
+    echo "Clang is already installed."
 fi
 
 # GLFW dependencies.
