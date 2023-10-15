05a64a6cc2e615bd1eae6e0c9f60c258f463b4df
Piotr Kocia <peter.kocia@gmail.com>
2023-10-15 10:35:25 +0200

    replace gl with glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
+#include <glad/glad.h>
+
+// GLAD must be included first. clang-format sorts includes, hence this comment
+// here.
+
 #include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[]) {
-  (void) argc;
-  (void) argv;
+int main(int argc, char* argv[])
+{
+  (void)argc;
+  (void)argv;
 
   // Initialize GLFW
-  if (!glfwInit()) {
+  if(!glfwInit()) {
     return -1;
   }
   // Create a GLFW window
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Hello GLFW", nullptr, nullptr);
+  GLFWwindow* window =
+    glfwCreateWindow(800, 600, "Hello GLFW", nullptr, nullptr);
 
-  if (!window) {
+  if(!window) {
     glfwTerminate();
     return -1;
   }
   glfwMakeContextCurrent(window);
   // Main loop
-  while (!glfwWindowShouldClose(window)) {
+  while(!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     glfwPollEvents();
