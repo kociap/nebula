be061bf34e8caba9f2a59fbb0c6b65b8b4bd75f4
sebawoz02 <sebawoz2002@gmail.com>
2024-01-24 18:18:25 +0100

    [UI] Renamed Movable_rect to movable_gate. Added ports to movable_gate.

--- src/ui/port.cpp
+++ src/ui/port.cpp
@@ -1,53 +1,70 @@
 #include <ui/port.hpp>
 
-namespace nebula::ui {
-    Port::Port(Vec2 const coordinates, port_t const type) {
-        this->coordinates = coordinates;
-        this->type = type;
+namespace nebula {
+  Port::Port(Vec2 const coordinates, port_t const type)
+  {
+    this->coordinates = coordinates;
+    this->type = type;
+  }
+
+  void Port::move(nebula::Vec2 const offset)
+  {
+    coordinates.x += offset.x;
+    coordinates.y += offset.y;
+  }
+
+  void Port::render() const
+  {
+    // Set color based on port type
+    if(type == IN) {
+      // Red for IN port
+    } else {
+      // Green for OUT port
     }
 
-    void Port::move(nebula::Vec2 const offset) {
-        coordinates.x += offset.x;
-        coordinates.y += offset.y;
-    }
+    // Draw circle shaped port
 
-    void Port::render() const {
-        // TODO: circle shaped port render function
-        // Draw dot
-
-        if(type == OUT){
-            // Draw connections
-        }
+    if(type == OUT) { // Draw connections
+      for(Port* p: connections) {
+        Vec2 sec_point = p->get_coordinates();
+        // Draw connection
+      }
     }
+  }
 
-    void Port::remove_connection(Port *old_port) {
-        List_Iterator<Port *, Port *> it;
-        for(it = connections.begin(); it != connections.end(); it++)
-        {
-            if(*it == old_port) {
-                connections.erase(it);
-                break;
-            }
-        }
+  void Port::remove_connection(Port* old_port)
+  {
+    List_Iterator<Port*, Port*> it;
+    for(it = connections.begin(); it != connections.end(); it++) {
+      if(*it == old_port) {
+        connections.erase(it);
+        break;
+      }
     }
+  }
 
-    void Port::add_connection(Port* new_port) {
-        // For IN type ports only one connection is allowed
-        if(type == IN && connections.size() > 0)
-        {
-            // Remove old connection
-            Port* old_port = *(connections.begin());
-            old_port->remove_connection(this);
-            connections.erase_front();
-        }
-        connections.emplace_front(new_port);
+  void Port::add_connection(Port* new_port)
+  {
+    // For IN type ports only one connection is allowed
+    if(type == IN && connections.size() > 0) {
+      // Remove old connection
+      Port* old_port = *(connections.begin());
+      old_port->remove_connection(this);
+      connections.erase_front();
     }
+    connections.emplace_front(new_port);
+  }
 
-    void Port::remove_all_connections() {
-        for(Port* port : connections)
-        {
-            port->remove_connection(this);
-        }
-        connections = {};
+  void Port::remove_all_connections()
+  {
+    for(Port* port: connections) {
+      port->remove_connection(this);
     }
-}
+    connections = {};
+  }
+
+  Vec2 Port::get_coordinates() const
+  {
+    return coordinates;
+  }
+} // namespace nebula
